1607ce3c472f267c35f9ae40d8c2277b2fd878f4
Martin Smith <martinsmith1968@gmail.com>
2017-11-06 15:31:28 +0000

    Added extension methods for all log4net call variants. Using t4 template for code and test generation

--- DNX.Helpers.Log4Net/LogManagerExtensions.cs
+++ DNX.Helpers.Log4Net/LogManagerExtensions.cs
@@ -3,13 +3,25 @@ using log4net;
 
 namespace DNX.Helpers.Log4Net
 {
+    /// <summary>
+    /// Class LogManagerExtensions.
+    /// </summary>
     public static class LogManagerExtensions
     {
+        /// <summary>
+        /// Gets the default logger.
+        /// </summary>
+        /// <returns>ILog.</returns>
         public static ILog GetDefaultLogger()
         {
             return LogManager.GetLogger(string.Empty);
         }
 
+        /// <summary>
+        /// Gets the logger for an object
+        /// </summary>
+        /// <param name="parent">The parent.</param>
+        /// <returns>ILog.</returns>
         public static ILog GetLoggerFor(object parent)
         {
             return parent == null
@@ -17,6 +29,10 @@ namespace DNX.Helpers.Log4Net
                 : LogManager.GetLogger(parent.GetType());
         }
 
+        /// <summary>
+        /// Gets the logger for where it's currently running (owner)
+        /// </summary>
+        /// <returns>ILog.</returns>
         public static ILog GetMyLogger()
         {
             var trace = new StackTrace();
@@ -28,6 +44,11 @@ namespace DNX.Helpers.Log4Net
                 : LogManager.GetLogger(caller.DeclaringType);
         }
 
+        /// <summary>
+        /// Gets a logger for the type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>ILog.</returns>
         public static ILog GetLogger<T>()
         {
             return LogManager.GetLogger(typeof(T));
